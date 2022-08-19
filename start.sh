80603ac976e1949acccc482e2c90c62bb25a9760
AK4111 <98637357+AK4111@users.noreply.github.com>
2022-08-19 08:59:12 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $SOURCE_CODE ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/kevinnadar22/URL-Shortener-V2.git /URL-Shortener-V2
+  git clone https://github.com/AK4111/link-convertor.git /link-convertor
 else
   echo "Cloning Custom Repo from $SOURCE_CODE "
-  git clone $SOURCE_CODE /URL-Shortener-V2
+  git clone $SOURCE_CODE /link-convertor
 fi
-cd /URL-Shortener-V2
+cd /link-convertor
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 -m main
